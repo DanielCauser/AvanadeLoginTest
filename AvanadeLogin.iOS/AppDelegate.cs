0ccdc25f230ac640d55c0d259e851e892a2b4245
Daniel Causer <danielcauser@gmail.com>
2020-11-17 09:16:41 -0500

    [DC] new ios proj

--- AvanadeLogin.iOS/AppDelegate.cs
+++ AvanadeLogin.iOS/AppDelegate.cs
@@ -6,7 +6,7 @@ namespace AvanadeLogin.iOS
     // The UIApplicationDelegate for the application. This class is responsible for launching the
     // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
     [Register("AppDelegate")]
-    public class AppDelegate : UIResponder, IUIApplicationDelegate //MvxApplicationDelegate<MvxIosSetup<Core.App>, Core.App>, IUIApplicationDelegate
+    public class AppDelegate : UIResponder, IUIApplicationDelegate
     {
 
         [Export("window")]
